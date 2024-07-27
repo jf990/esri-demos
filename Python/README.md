c366c6ad9beeb3579c5f2621af9e0b50ec63626f
John Foster <jfoster@esri.com>
2024-07-26 19:36:28 -0700

    Add python item copy script

--- Python/README.md
+++ Python/README.md
@@ -1,3 +1,4 @@
 # ArcGIS API for Python
 
 * [Basic Python demo](./basic-demo.html), load the notebook (ipynb) file as a Jupyter Notebook.
+* `copyitem.py` Python CLI script to copy items from one ArcGIS Organization to another ArcGIS Organization.
---
+++ Python/copyitem.py
@@ -0,0 +1,26 @@
+# Python script to copy an item, such as a feature service, from one ArcGIS Online Organization to another.
+# You must have an account in both orgs, and the item id of the source item to copy.
+# Run this with `python copyitem.py`
+
+from arcgis.gis import GIS
+
+username_source = input("Enter user name in source organization: ")
+gis_source=GIS(r"https://arcgis.com", username_source)
+
+username_target = input("Enter user name in target organization: ")
+gis_target = GIS(r"https://arcgis.com", username_target)
+
+item_id = input("Enter item id from source organization to copy: ")
+items = gis_source.content.search(item_id) 
+
+def deep_copy_items(item_list):
+    for item in item_list:
+        try:
+            print("Cloning " + item.title)
+            gis_target.content.clone_items(items=[item], copy_data=True, search_existing_items=True)
+            print("Successfully cloned " + item.title)
+        except Exception as e:
+            print(e)
+    print("Copy complete")
+
+deep_copy_items(items)
